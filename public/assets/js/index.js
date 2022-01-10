381ade6bd9eb1c2ce631c7b948e03307837fdd3a
michkw0708 <michael.kwan80@gmail.com>
2022-01-10 21:19:30 +0700

    sudah finalllll
    Co-authored-by: Rafli Athala Jaskandi <rafli.jaskandi@gmail.com>

---
+++ public/assets/js/index.js
@@ -0,0 +1,27 @@
+let slideWidth = $('.sliderContainer').width()
+$('#slides').css("margin-left", -slideWidth)
+$('#slides:last-child').prependTo("#slides")
+
+$('#leftBtn').click(() => {
+    console.log('Left Button Clicked')
+    $('#slides').animate(
+        {"left": slideWidth},
+        1000,
+        () => {
+            $('#slide:last-child').prependTo("#slides")
+            $('#slides').css("left","")
+        }
+    )
+})
+
+$('#rightBtn').click(()=> {
+    console.log('Right Button Clicked')
+    $('#slides').animate(
+        {"left": -slideWidth},
+        1000,
+        () => {
+            $('#slide:first-child').appendTo("#slides")
+            $('#slides').css("left","")
+        }
+    )
+})
