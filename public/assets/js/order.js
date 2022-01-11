d2ff2833e70040f9fe787f8c4310ec2212581349
Rafli Athala Jaskandi <rafli.jaskandi@binus.ac.id>
2022-01-11 09:38:16 +0700

    Final change
    - Project requirement is complete
    
    - Fix order behaviour based on menu listed
    
    - Show alert when user leaving order page before finishing order
    
    - Input order on list when select order from menu in index or menu page

--- public/assets/js/order.js
+++ public/assets/js/order.js
@@ -1,6 +1,14 @@
 // ==============
 // Place Order
 // ==============
+let menu = [
+    { menuId: 1, name: 'Family Pack for 5 people', img: '/public/assets/img/bestseller1.png' },
+    { menuId: 2, name: 'Nasi King Box Special', img: '/public/assets/img/bestseller2.png' },
+    { menuId: 3, name: 'Cheese BuRHger King', img: '/public/assets/img/bestseller3.png' },
+    { menuId: 4, name: 'Kids Box Special Blacpink', img: '/public/assets/img/bestseller4.png' },
+    { menuId: 5, name: 'Beef Special BuRHger XL', img: '/public/assets/img/bestseller5.png' },
+    { menuId: 6, name: 'Special BuRHger King', img: '/public/assets/img/bestseller6.png' }
+]
 let orders = []
 $('.menu > .card-body > a').each((i, e) => {
     e.addEventListener('click', (e) => {
@@ -14,12 +22,12 @@ $('.menu > .card-body > a').each((i, e) => {
         $('.orders > .card-body > div').html('')
         orders.forEach((val, idx, arr) => {
             $('.orders > .card-body > div')
-            .append(`
+                .append(`
                 <div class="d-flex justify-content-between align-items-center order order-${val.menuId}" id="menu-${val.menuId}">
                     <div class="d-flex align-items-center">
-                        <img class="mr-3 " src="assets/img/amirali-mirhashemian-sc5sTPMrVfk-unsplash.jpg" alt=""
+                        <img class="mr-3" src="${menu[val.menuId - 1].img}" alt=""
                             height="100">
-                        <p><b>Menu ${val.menuId}</b></p>
+                        <p><b>${menu[val.menuId - 1].name}</b></p>
                         <input
                             type="hidden"
                             name="order-id"
@@ -82,7 +90,6 @@ $('.menu > .card-body > a').each((i, e) => {
                 // console.log(orders)
             })
         })
-
     })
 })
 
@@ -159,3 +166,100 @@ $('.order-form').submit((e) => {
         $('.order-form').addClass('was-validated')
     }
 })
+
+// ===============
+// Redirect Behaviour
+// ===============
+
+const urlParams = new URLSearchParams(window.location.search)
+if (
+    urlParams.has('id') && 
+    (menu.map((e) => { return e.menuId }).indexOf(parseInt(urlParams.get('id'))) !== -1)
+) {
+    orders.push({
+        menuId: urlParams.get('id'),
+        quantity: 1
+    })
+    // DEBUG
+    // console.log(orders)
+    $('.order-form > input[name=\"isEmpty\"]').attr("value", "false")
+    $('.orders > .card-body > div').html('')
+    orders.forEach((val, idx, arr) => {
+        $('.orders > .card-body > div')
+            .append(`
+            <div class="d-flex justify-content-between align-items-center order order-${val.menuId}" id="menu-${val.menuId}">
+                <div class="d-flex align-items-center">
+                    <img class="mr-3" src="${menu[val.menuId - 1].img}" alt=""
+                        height="100">
+                    <p><b>${menu[val.menuId - 1].name}</b></p>
+                    <input
+                        type="hidden"
+                        name="order-id"
+                        value="menu-${val.menuId}"
+                    >
+                </div>
+                <div class="d-flex align-items-center justify-content-between">
+                    <button class="mr-1 btn btn-primary" id="decrement" type="button">-</button>
+                    <input 
+                        class="text-center mx-1" 
+                        type="number" name="menu-quantity-${val.menuId}" 
+                        id="menu-quantity-${val.menuId}"
+                        value="1"
+                        min="1"
+                        max="100"
+                        required
+                        >
+                    <button class="mx-1 btn btn-primary" id="increment" type="button" >+</button>
+                    <button class="ml-1 btn btn-danger" id="delete" type="button"><i class="fas fa-trash-alt"></i></button>
+                </div>
+            </div>
+        `)
+        $(`.order-${val.menuId} #decrement`).click((e) => {
+            if ($(`.order-${val.menuId} input[type=\"number\"]`).attr("value") > 1) {
+                $(`.order-${val.menuId} input[type=\"number\"]`).attr("value", (parseInt($(`.order-${val.menuId} input[type=\"number\"]`).attr("value")) - 1).toString())
+
+                orders[
+                    orders.map((e) => { return e.menuId })
+                        .indexOf(val.menuId)
+                ].quantity -= 1
+                // DEBUG
+                // console.log(orders)
+            }
+        })
+        $(`.order-${val.menuId} #increment`).click((e) => {
+            if ($(`.order-${val.menuId} input[type=\"number\"]`).attr("value") < 100) {
+                $(`.order-${val.menuId} input[type=\"number\"]`).attr("value", (parseInt($(`.order-${val.menuId} input[type=\"number\"]`).attr("value")) + 1).toString())
+
+                orders[
+                    orders.map((e) => { return e.menuId })
+                        .indexOf(val.menuId)
+                ].quantity += 1
+                // DEBUG
+                // console.log(orders)
+            }
+
+        })
+        $(`.order-${val.menuId} #delete`).click((e) => {
+            $(`#menu-${val.menuId}`).remove()
+            orders.splice(
+                orders.map((e) => { return e.menuId })
+                    .indexOf(val.menuId),
+                1
+            )
+            if (orders.length === 0) {
+                $('.orders > div > div').html('<p>No order listed</p>')
+                $('.orders > input[name=\"isEmpty\"]').attr("value", "true")
+            }
+            // DEBUG
+            // console.log(orders)
+        })
+    })
+}
+
+if (orders.length !== 0) {
+    $('.nav-item > a').click((e) => {
+        if (!confirm('Please finish the order, if you move to another page, your process is not saved!')) {
+            e.preventDefault();
+        }
+    })
+}
